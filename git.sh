10c991a03063a61b80125d82c933f0a53a37ec27
Louis Zhang <lolkokpop@126.com>
2018-10-30 22:48:37 +0800

    Git: add git remote cmd
    Signed-off-by: Louis Zhang <lolkokpop@126.com>

--- git.sh
+++ git.sh
@@ -6,3 +6,12 @@ git checkout tagname -b branch_name
 #delete:
 git br -d branch_name #auto merge first before delete
 git br -D branch_name #force to delete
+
+#add local code to remote null respority
+git remote add origin git@github.com:LouisZhangCN/xx.git
+# if remote respority isnot null
+git pull --rebase origin master
+git push -u origin master
+
+#check remote
+git remote -v
